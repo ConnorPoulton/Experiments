e085c24a5f8eacc78aae6e41a59fa6ba258392be
ConnorPoulton <poulton.cw@gmail.com>
2015-12-15 11:01:47 -0800

    My delegate lesson part 1

--- UnityE/Assets/Delegates.cs
+++ UnityE/Assets/Delegates.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Delegates : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public delegate int DelegateX(int x);//Declare delegate type
+
+	static int SquareNumber(int x) //the function we want to add to the delegate
+	{
+		return x * x;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	static int DoubleNumber(int x) //another function to add to the delegate
+	{
+		return x * 2;
 	}
+
+	DelegateX squared = new DelegateX(SquareNumber); //create instance of the delegate
+	DelegateX doubled = new DelegateX(DoubleNumber);
+
+	//now we can call the functions SquareNumber and DoubleNumber by passing them as variables of type DelegateX
+
+	void ReadDelegates(DelegateX delegatex)
+	{
+		Debug.Log (delegatex (4));
+	}
+
+	void Start()
+	{
+		ReadDelegates (squared);
+	}
+
 }
